c9be0172bc66ebd4601819a534b1e882769ff708
doyan-dev <db9326300292@gmail.com>
2026-09-19 12:03:51 +0530

    .

---
+++ src/level4.ts
@@ -0,0 +1,13 @@
+//type of objects
+
+type user={
+    name:string,
+    age:number                  //type of the object is defined
+    
+}
+
+let a:user;
+a={
+    name:"doyan",
+    age:22
+}
---
+++ src/llevel3.ts
@@ -0,0 +1,14 @@
+//making own type
+type db= string|number;
+
+
+let a:db="abc";
+console.log(a);
+a=2;                       //a can only hold string or number
+                          // called type alias
+console.log(a);
+
+
+type status= "pending" |"done";
+let work:status= "pending";
+console.log(work);
